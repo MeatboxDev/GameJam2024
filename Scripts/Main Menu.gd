40d8a844188248fa9902b9d8bce5a74fe92eb994
ClaraCF <78223334+ClaraCF@users.noreply.github.com>
2024-01-26 13:59:47 -0400

    Modify Viewport resolution and add Main Menu

---
+++ Scripts/Main Menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+# Called when pressing the Quit button on the Main Menu
+func _on_quit_button_pressed():
+	get_tree().quit()
