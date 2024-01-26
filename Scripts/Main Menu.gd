7cab33302b49f7f6d7785a7db8b3817c047c3f98
ruskki <theawsomegirl2004@gmail.com>
2024-01-26 18:57:53 -0400

    main menu2

--- Scripts/Main Menu.gd
+++ Scripts/Main Menu.gd
@@ -11,6 +11,13 @@ func _process(delta):
 	pass
 
 
+
+func _on_play_button_pressed():
+	get_tree().change_scene_to_file("res://Maps/Maps/cave_map.tscn")
+
+
+
 # Called when pressing the Quit button on the Main Menu
 func _on_quit_button_pressed():
 	get_tree().quit()
+
