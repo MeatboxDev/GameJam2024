caa71fd901872a7204ef122680cff0e4fdf118d0
ClaraCF <78223334+ClaraCF@users.noreply.github.com>
2024-01-26 19:36:51 -0400

    Amiguito climbing tutorial

--- GameObject/Player/Player.gd
+++ GameObject/Player/Player.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
+var jumping = false
 
 @export var SPEED = 300.0
-@export var JUMP_VELOCITY = -800.0
+@export var JUMP_VELOCITY = -600.0
+@export var JUMP_ACCELERATION = 100.0
 @export var DEACCEL_FACTOR = 0.9
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -12,10 +14,18 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
+	
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_pressed("jump") and is_on_floor():
+		jumping = true
+	elif Input.is_action_just_released("jump"):
+		jumping = false
+		
+	if jumping:
+		if velocity.y >= JUMP_VELOCITY:
+			velocity.y -= JUMP_ACCELERATION
+		else:
+			jumping = false
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -30,3 +40,4 @@ func _physics_process(delta):
 		velocity.x *= DEACCEL_FACTOR
 
 	move_and_slide()
+	
