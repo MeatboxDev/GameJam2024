7eeb43668f272cfdc43ddeb0e505b3824ee22226
ClaraCF <78223334+ClaraCF@users.noreply.github.com>
2024-01-27 04:56:58 -0400

    Experiment with controller detection

--- Maps/Maps/cave_map.gd
+++ Maps/Maps/cave_map.gd
@@ -6,8 +6,17 @@ func _ready():
 	const player_scene = preload("res://GameObject/Player/Player.tscn")
 	const boykisser_scene = preload("res://GameObject/Player/Boykisser.tscn")
 	
-	# Controllers
-	var connected_controllers = Input.get_connected_joypads()
+	# Add all unique controllers we can detect
+	var connected_controllers = []
+	for i in Input.get_connected_joypads():
+		var new_controller = true
+		print(Input.get_joy_guid(i))
+		for j in connected_controllers:
+			if Input.get_joy_guid(i) == Input.get_joy_guid(j):
+				new_controller = false
+				break
+		if new_controller:
+			connected_controllers.append(i)
 	
 	print("%d connected controllers" % connected_controllers.size())
 	for i in connected_controllers:
@@ -15,19 +24,10 @@ func _ready():
 		
 		# Add a player per controller
 		# Probably will remove later
-		#var player = player_scene.instantiate()
-		#player.player_index = i
-		#add_child(player)
+		var player = player_scene.instantiate()
+		player.player_index = i
+		add_child(player)
 		
-	# Player 1
-	var player1 = player_scene.instantiate()
-	player1.player_index = 0
-	add_child(player1)
-
-	# Player 2
-	var player2 = boykisser_scene.instantiate()
-	player2.player_index = 2
-	add_child(player2)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
