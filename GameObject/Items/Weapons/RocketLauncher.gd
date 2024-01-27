e7da26e37375eaa6df0f69e589c87115d4030ded
Kimbix <alemanhumberto06@gmail.com>
2024-01-26 21:53:49 -0400

    Test

--- GameObject/Items/Weapons/RocketLauncher.gd
+++ GameObject/Items/Weapons/RocketLauncher.gd
@@ -2,7 +2,7 @@ extends Node
 
 const rocket = preload("res://GameObject/Items/Projectiles/Rocket.tscn")
 
-func Activate((float)direction):
+func Activate(direction):
 	var rocket_instance = rocket.instantiate()
 	get_parent().add_child(rocket_instance)
 	rocket_instance.find_child("Script").speed = Vector2(direction * 100, -5)
