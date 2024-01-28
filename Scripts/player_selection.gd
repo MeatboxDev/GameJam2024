9ae35aed1c291f1c662ec3c16c17caca73a75c32
ClaraCF <78223334+ClaraCF@users.noreply.github.com>
2024-01-28 05:39:18 -0400

    Grunch

--- Scripts/player_selection.gd
+++ Scripts/player_selection.gd
@@ -1,10 +1,30 @@
 extends Node2D
 
+# Player Entries
+const player1_entry = preload("res://Scenes/PlayerSelect/PlayerEntries/Player1_Entry.tscn")
+const player2_entry = preload("res://Scenes/PlayerSelect/PlayerEntries/Player2_Entry.tscn")
+const player3_entry = preload("res://Scenes/PlayerSelect/PlayerEntries/Player3_Entry.tscn")
+const player4_entry = preload("res://Scenes/PlayerSelect/PlayerEntries/Player4_Entry.tscn")
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	# Instantiate an entry for all players
+	var player1 = player1_entry.instantiate()
+	var player2 = player2_entry.instantiate()
+	var player3 = player3_entry.instantiate()
+	var player4 = player4_entry.instantiate()
 
+	# Start everybody's dancing animation
+	player1.find_child("AnimationPlayer").play("RedDance")
+	player2.find_child("AnimationPlayer").play("BlueDance")
+	player3.find_child("AnimationPlayer").play("GreenDance")
+	player4.find_child("AnimationPlayer").play("GrayDance")
+	
+	find_child("PlayersContainer").add_child(player1)
+	find_child("PlayersContainer").add_child(player2)
+	find_child("PlayersContainer").add_child(player3)
+	find_child("PlayersContainer").add_child(player4)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
